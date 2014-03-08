045bbd6d1d554f85a0db760fb7357984a527c306
domepik <domepik@gmail.com>
2014-03-08 15:39:38 -0500

    Update and rename sound-snatch.php to sound-snatch.py

---
+++ sound-snatch.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+
+import urllib.request
+import requests
+import sys
+import re
+
+apikey = '' # API Key goes here.
+track = input('Track URL> ')
+
+def getTrackInfo(track):
+    request = requests.get("http://api.soundcloud.com/resolve.json?url={0}&client_id={1}".format(track, apikey))
+    return request.json()
+
+def getTrackDownloadLink(trackid):
+    request = requests.get("http://api.soundcloud.com/i1/tracks/{0}/streams?client_id={1}".format(trackid, apikey))
+    return request.json()['http_mp3_128_url']
+
+
+track_id = getTrackInfo(track)['id']
+track_title = getTrackInfo(track)['title']
+track_download = getTrackDownloadLink(track_id)
+
+urllib.request.urlretrieve(track_download, "{0}.mp3".format(track_title))
+
+sys.exit() 
